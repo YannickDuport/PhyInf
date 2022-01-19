ba24bebba0b94489930c4b67867edf4069467a36
yannickduport <yannid86@zedat.fu-berlin.de>
2022-01-19 23:20:22 +0100

    paths added

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -4,4 +4,5 @@ ROOT_PATH = Path(__file__).parent.parent
 DATA_PATH = ROOT_PATH / "data"
 TREE_PATH = DATA_PATH / "trees"
 TREE_MINI_PATH = DATA_PATH / "trees_mini"
+SEQ_MINI_PATH = DATA_PATH / 'sequences_mini'
 
